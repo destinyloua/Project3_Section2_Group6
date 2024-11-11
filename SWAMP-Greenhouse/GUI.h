1171f4aee61c82477814e42f7391c0d8412deb52
LIAM <lcabarle6383@conestogac.on.ca>
2024-11-11 00:08:23 -0500

    GUI initial Implementation 3

--- SWAMP-Greenhouse/GUI.h
+++ SWAMP-Greenhouse/GUI.h
@@ -6,17 +6,16 @@
 class GUI
 {
 private:
-	//int id;
-	//int userId;
+	int id;
+	int userId;
 	void DrawCameraControls();
 	void DrawPanels();
 public:
 	GUI();
-	~GUI();
-	//bool Display();
+	bool Login(System& system);
+	void Panel(Device& device);
+	void DisplayAlert(Device& device);
 	void Init();
 	void UpdateDrawing();
-	//bool Login(System& system);
-	//void Panel(Device& device);
-	//void DisplayAlert(Device& device);
+	~GUI();
 };
