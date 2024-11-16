2d6459f8534cd6519a62fd00d1df4a5a97b1af90
aiden wilson <awilson9717@conestogac.on.ca>
2024-11-16 10:22:13 -0500

    light class, and fix temp error

--- SWAMP-Greenhouse/Lights.h
+++ SWAMP-Greenhouse/Lights.h
@@ -1,5 +1,24 @@
 #pragma once
-class Lights
+
+#include "Device.h"
+#include <string>
+#include <iostream>
+
+class Lights : public Device
 {
+	string lightType; // LED
+	double minLightLevel;
+	double maxLightLevel;
+
+public:
+	Lights(string lightType, double minLightLevel, double maxLightLevel);
+	string getLightType();
+	double getminLightLevel();
+	double getmaxLightLevel();
+	string setLightType(string lightType);
+	double getminLightLevel(double minLightLevel);
+	double getmaxLightLevel(double maxLightLevel);
+	void setScheduel(int StartTime, int StopTime);
+	~Lights();
 };
 
