59c81a6236843d1481d5f944d54f8fbd51f362d8
DESTINY LOUANGSOMBATH <dlouangsombath5708@conestogac.on.ca>
2024-11-23 10:40:28 -0500

    create unit test suite

---
+++ SWAMP_TESTS/SWAMP_TESTS.cpp
@@ -0,0 +1,21 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../SWAMP-Greenhouse/CO2.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace SWAMPTESTS
+{
+	TEST_CLASS(SWAMPTESTS)
+	{
+	public:
+		
+		// Testing CO2 reads data from file accurately -- req #1
+		TEST_METHOD(CO2_Test01_Req01)
+		{
+			CO2 c;
+	
+			
+		}
+	};
+}
