d412850116b076cfb751282a0a11e3d158724116
TylerDdao <baonamdao05@gmail.com>
2024-11-30 13:27:17 -0500

    Unit Test

--- SWAMP_TESTS/SWAMP_TESTS.cpp
+++ SWAMP_TESTS/SWAMP_TESTS.cpp
@@ -1,6 +1,5 @@
-#include "pch.h"
 #include "CppUnitTest.h"
-#include "../SWAMP-Greenhouse/CO2.h"
+#include "../SWAMP-Greenhouse/SoilMoisture.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,13 +8,49 @@ namespace SWAMPTESTS
 	TEST_CLASS(SWAMPTESTS)
 	{
 	public:
-		
-		// Testing CO2 reads data from file accurately -- req #1
-		TEST_METHOD(CO2_Test01_Req01)
+		TEST_METHOD(Soil_UnitTest01_REQ01)
 		{
-			CO2 c;
-	
-			
+			SoilMoisture s;
+			s.readData();
+			s.control();
+			Assert::IsTrue(s.GetIrrigation());
+		}
+		TEST_METHOD(Soil_UnitTest01_REQ02)
+		{
+			SoilMoisture s;
+			s.SetIrrigationStatus();
+			Assert::IsTrue(s.GetIrrigation());
+		}
+		TEST_METHOD(Soil_UnitTest01_REQ03)
+		{
+			SoilMoisture s;
+			s.readData();
+			s.control();
+			Assert::IsTrue(s.GetIrrigation());
+		}
+		TEST_METHOD(Soil_UnitTest01_REQ04)
+		{
+			SoilMoisture s;
+			s.readData();
+			s.control();
+			Assert::IsTrue(s.GetIrrigation());
+		}
+		TEST_METHOD(Soil_UnitTest01_REQ05)
+		{
+			SoilMoisture s;
+			s.IncreaseThreshold();
+			Assert::AreEqual(31.00,s.GetThreshold());
+		}
+		TEST_METHOD(Soil_UnitTest02_REQ05)
+		{
+			SoilMoisture s;
+			s.DecreaseThreshold();
+			Assert::AreEqual(29.00, s.GetThreshold());
+		}
+		TEST_METHOD(Soil_UnitTest01_REQ06)
+		{
+			SoilMoisture s;
+			Assert::AreEqual(30.00, s.GetThreshold());
 		}
 	};
 }
